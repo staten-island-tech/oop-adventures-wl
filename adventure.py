fdc54bddcaeca842d9a392e4fb46eac98130b7e3
WilliamLi0430 <WilliamL355@nycstudents.net>
2026-01-05 09:18:52 -0500

    Create adventure.py

---
+++ adventure.py
@@ -0,0 +1,9 @@
+import time
+import random
+
+class damage():
+    def __init__(self, health, damage, ability, inventory):
+        health.self = health
+        damage.self = damage
+        ability.self = ability
+        inventory.self = inventory
