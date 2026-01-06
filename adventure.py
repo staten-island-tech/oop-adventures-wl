df74399dbbb5775447b9e96c8d113189c4618d35
WilliamLi0430 <WilliamL355@nycstudents.net>
2026-01-06 09:11:23 -0500

    Update adventure.py

--- adventure.py
+++ adventure.py
@@ -1,9 +1,18 @@
 import time
 import random
 
+class enemy1():
+    def __init__(self, health, damage):
+        self.health = health
+        self.damage = damage
+
 class damage():
     def __init__(self, health, damage, ability, inventory):
-        health.self = health
-        damage.self = damage
-        ability.self = ability
-        inventory.self = inventory
+        self.health = health
+        self.damage = damage
+        self.ability = ability
+        self.inventory = inventory
+
+goblin = enemy1("johnny", 100, 10)
+luke = damage("Luke", 100, 50, "Penguin Slap", [])
+
