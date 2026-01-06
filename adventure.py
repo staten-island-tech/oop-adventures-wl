b6c08e6b2bca58f21678f76fb74fbb8e5ece035d
lukezheng2011 <lukezheng2011@gmail.com>
2026-01-06 09:26:22 -0500

    sdgsgsdgdggsdgsdgsdg
    sdgsdgsgsdg

--- adventure.py
+++ adventure.py
@@ -1,20 +1,4 @@
-import time
-import random
+# ive done one line
+print("I've done one line")
 
-class enemy1():
-    def __init__(self, health, damage):
-        self.health = health
-        self.damage = damage
-
-class damage():
-    def __init__(self, health, damage, ability, inventory):
-        self.health = health
-        self.damage = damage
-        self.ability = ability
-        self.inventory = inventory
-
-goblin = enemy1("johnny", 100, 10)
-luke = damage("Luke", 100, 50, "Penguin Slap", [])
-
-ability = []
-
+print("I feel amazing")
