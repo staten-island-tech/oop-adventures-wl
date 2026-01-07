075e08b0d12ec90020461eb51c1fd6fb837f1c89
lukezheng2011 <lukezheng2011@gmail.com>
2026-01-07 09:26:01 -0500

    aaaa
    aaaaaaaaaaaaaa

--- adventure.py
+++ adventure.py
@@ -1,5 +1,35 @@
-# ive done one line
-print("I've done one line")
+import time
+import random
 
-print("I feel amazing")
+class enemy1():
+    def __init__(self, name, health, damage):
+        self.name = name
+        self.health = health
+        self.damage = damage
 
+class damage():
+    def __init__(self, name, health, damage, ability, inventory):
+        self.name = name
+        self.health = health
+        self.damage = damage
+        self.ability = ability
+        self.inventory = inventory
+    def giveAbi(self):
+        number = [0, 1, 2, 3, 4]
+        self = damage("Luke", 100, 50, random.choice(number), ["vial"])
+        if number == ("0", "1", "2", "3", "4"):
+            self.ability == ("penguin slap")
+        else:
+            print("error")
+        print(self.__dict__)
+
+
+giveAbi()
+
+
+
+# number = [0, 1, 2, 3, 4]
+# luke = damage("Luke", 100, 50, random.choice(number), ["vial"])
+# if number == ("0", "1", "2", "3", "4"):
+#     luke.ability == ("penguin slap")
+# print(luke.__dict__)
