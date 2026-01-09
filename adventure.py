e9d59bea9a0098211c015e49e84dff9220de1c73
WilliamLi0430 <WilliamL355@nycstudents.net>
2026-01-09 09:15:10 -0500

    Update adventure.py

--- adventure.py
+++ adventure.py
@@ -7,56 +7,109 @@ class Enemy1():
         self.health = health
         self.damage = damage
 
-class Poison():
-    def __init__(self, name, health, damage, ability, inventory):
+#Falcon is sniper
+class Falcon():
+    def __init__(self, name, health, damage, shield, ability, inventory):
+        ability = [
+            "Falcon Dive",
+            "Falcon Feathers",
+            "Falcon Screech",
+            "Falcon Claw",
+            "Falcon Beak"
+        ]
         self.name = name
         self.health = health
         self.damage = damage
-        self.ability = ability
+        self.shield = shield
+        giveabi = random.choice(ability)
+        self.ability = giveabi
         self.inventory = inventory
 
-class Support():
-    def __init__(self, name, health, damage, ability, inventory):
+#Snake is poison
+class Snake():
+    def __init__(self, name, health, damage, shield, ability, inventory):
+        ability = [
+            "Snake Venom",
+            "Snake Crush",
+            "Snake Fangs",
+            "Snake Hiss"
+        ]
         self.name = name
         self.health = health
         self.damage = damage
-        self.ability = ability
+        self.shield = shield
+        giveabi = random.choice(ability)
+        self.ability = giveabi
         self.inventory = inventory
 
-class Tank():
-    def __init__(self, name, health, damage, ability, inventory):
+#Frog is support
+class Frog():
+    def __init__(self, name, health, damage, shield, ability, inventory):
+        ability = [
+            "Frog Boost", #Raises damage
+            "Frog Assist", #Frog also attacks
+            "Frog Vial" #Raises health
+        ]
         self.name = name
         self.health = health
         self.damage = damage
-        self.ability = ability
+        self.shield = shield
+        giveabi = random.choice(ability)
+        self.ability = giveabi
+        self.inventory = inventory
+
+#Shark is tank
+class Shark():
+    def __init__(self, name, health, damage, shield, ability, inventory):
+        ability = [
+            "Shark Attack",
+            "Shark Fin",
+            "Shark Tail Swing",
+            "Shark Skin",
+            "Shark Teeth"
+        ]
+        self.name = name
+        self.health = health
+        self.damage = damage
+        self.shield = shield
+        giveabi = random.choice(ability)
+        self.ability = giveabi
         self.inventory = inventory
 
 #Bunny is healer
 class Bunny():
-    def __init__(self, name, health, damage, ability, inventory):
+    def __init__(self, name, health, damage, shield, ability, inventory):
+        ability = [
+            "Bunny Heal",
+            "Bunny Shield",
+            "Bunny Protection"
+        ]
         self.name = name
         self.health = health
         self.damage = damage
-        self.ability = ability
+        self.shield = shield
+        giveabi = random.choice(ability)
+        self.ability = giveabi
         self.inventory = inventory
 
 #Penguin is damage
 class Penguin():
-    def __init__(self, name, health, damage, ability, inventory):
+    def __init__(self, name, health, damage, shield, ability, inventory):
         ability = [
             "Penguin Slap",
             "Penguin Punch",
-            "Penguin Strike",
+            "Penguin Slide",
             "Penguin Beak Attack",
             "Penguin Stomp"
         ]
         self.name = name
         self.health = health
         self.damage = damage
-        giveAbi = random.choice(ability)
-        self.ability = giveAbi
+        self.shield = shield
+        giveabi = random.choice(ability)
+        self.ability = giveabi
         self.inventory = inventory
 
-luke = Penguin("Luke", 100, 50, "", ["Vial"])
+luke = Penguin("Luke", 100, 50, 0, "", [""])
 print(luke.__dict__)
 print("hello")
