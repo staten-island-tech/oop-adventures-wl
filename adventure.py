eb09bf3d44e61e46e9ffaef1fddd6fa560f608ca
WilliamLi0430 <WilliamL355@nycstudents.net>
2026-01-14 09:26:13 -0500

    Update adventure.py

--- adventure.py
+++ adventure.py
@@ -1,7 +1,8 @@
 import time
 import random
 
-class Enemy1():
+#Wow an enemy
+class Enemy():
     def __init__(self, name, health, damage):
         self.name = name
         self.health = health
@@ -9,8 +10,8 @@ class Enemy1():
 
 #Cat is hypnotist
 class Cat():
-    def __init__(self, name, health, damage, shield, ability, inventory):
-        ability = [
+    def __init__(self, name, health, damage, shield, abilities, inventory):
+        abilities = [
             "Cat Meow",
             "Cat Claws",
             "Cat Eyes",
@@ -20,13 +21,13 @@ class Cat():
         self.health = health
         self.damage = damage
         self.shield = shield
-        self.ability = ability
+        self.abilities = abilities
         self.inventory = inventory
 
 #Falcon is sniper
 class Falcon():
-    def __init__(self, name, health, damage, shield, ability, inventory):
-        ability = [
+    def __init__(self, name, health, damage, shield, abilities, inventory):
+        abilities = [
             "Falcon Dive",
             "Falcon Feathers",
             "Falcon Screech",
@@ -37,13 +38,13 @@ class Falcon():
         self.health = health
         self.damage = damage
         self.shield = shield
-        self.ability = ability
+        self.abilities = abilities
         self.inventory = inventory
 
 #Snake is poison
 class Snake():
-    def __init__(self, name, health, damage, shield, ability, inventory):
-        ability = [
+    def __init__(self, name, health, damage, shield, abilities, inventory):
+        abilities = [
             "Snake Venom",
             "Snake Crush",
             "Snake Fangs",
@@ -53,13 +54,13 @@ class Snake():
         self.health = health
         self.damage = damage
         self.shield = shield
-        self.ability = ability
+        self.abilities = abilities
         self.inventory = inventory
 
 #Frog is support
 class Frog():
-    def __init__(self, name, health, damage, shield, ability, inventory):
-        ability = [
+    def __init__(self, name, health, damage, shield, abilities, inventory):
+        abilities = [
             "Frog Boost", #Raises damage
             "Frog Assist", #Frog also attacks
             "Frog Vial" #Raises health
@@ -68,13 +69,13 @@ class Frog():
         self.health = health
         self.damage = damage
         self.shield = shield
-        self.ability = ability
+        self.abilities = abilities
         self.inventory = inventory
 
 #Shark is tank
 class Shark():
-    def __init__(self, name, health, damage, shield, ability, inventory):
-        ability = [
+    def __init__(self, name, health, damage, shield, abilities, inventory):
+        abilities = [
             "Shark Attack",
             "Shark Fin",
             "Shark Tail Swing",
@@ -85,13 +86,13 @@ class Shark():
         self.health = health
         self.damage = damage
         self.shield = shield
-        self.ability = ability
+        self.abilities = abilities
         self.inventory = inventory
 
 #Bunny is healer
 class Bunny():
-    def __init__(self, name, health, damage, shield, ability, inventory):
-        ability = [
+    def __init__(self, name, health, damage, shield, abilities, inventory):
+        abilities = [
             "Bunny Heal",
             "Bunny Shield",
             "Bunny Protection"
@@ -100,13 +101,13 @@ class Bunny():
         self.health = health
         self.damage = damage
         self.shield = shield
-        self.ability = ability
+        self.abilities = abilities
         self.inventory = inventory
 
 #Penguin is damage
 class Penguin():
-    def __init__(self, name, health, damage, shield, ability, inventory):
-        ability = [
+    def __init__(self, name, health, damage, shield, abilities, inventory):
+        abilities = [
             "Penguin Slap",
             "Penguin Punch",
             "Penguin Slide",
@@ -117,13 +118,40 @@ class Penguin():
         self.health = health
         self.damage = damage
         self.shield = shield
-        self.ability = ability
+        self.abilities = abilities
         self.inventory = inventory
 
-luke = Penguin("Luke", 100, 50, 0, "", [""])
+
+classify = input("What class would you like to be? Damage, Tank, Healer, Support, Poison, Sniper, Hypnotist. ")
+if classify == ("Damage") or ("damage"):
+    naming = input("What is your name? ")
+    luke = Penguin(naming, 100, 10, 0, "", [""])
+if classify == ("Tank") or ("tank"):
+    naming = input("What is your name? ")
+    luke = Shark(naming, 100, 10, 0, "", [""])
+elif classify == ("Healer") or ("healer"):
+    naming = input("What is your name? ")
+    luke = Bunny(naming, 100, 10, 0, "", [""])
+elif classify == ("Support") or ("support"):
+    naming = input("What is your name? ")
+    luke = Frog(naming, 100, 10, 0, "", [""])
+elif classify == ("Poison") or ("poison"):
+    naming = input("What is your name? ")
+    luke = Snake(naming, 100, 10, 0, "", [""])
+elif classify == ("Sniper") or ("sniper"):
+    naming = input("What is your name? ")
+    luke = Falcon(naming, 100, 10, 0, "", [""])
+elif classify == ("Hypnotist") or ("hypnotist"):
+    naming = input("What is your name? ")
+    luke = Cat(naming, 100, 10, 0, "", [""])
+else:
+    print("Class not found")
+
+goblin = Enemy("Goblin", 75, 10)
 print(luke.__dict__)
-print("hello")
 
 
 def battle():
-    print(f"{luke.self.name}")
+    print(f"{luke.name} is now fighting {goblin.name}!")
+
+battle()
