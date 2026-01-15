16fcea43f3194d26dc97b0002abe8a9bd71a051e
lukezheng2011 <lukezheng2011@gmail.com>
2026-01-14 20:57:01 -0500

    fix the dictionaries with the abilities
    aaaaaa

--- adventure.py
+++ adventure.py
@@ -9,24 +9,24 @@ class Enemy():
 
 #Cat is hypnotist
 class Cat():
-    def __init__(self, name, health, damage, shield, abilities, inventory):
-        abilities = [
+    def __init__(self, name, health, damage, shield, inventory):
+        self.abilities = [
             {"abilityName":"Cat Meow","dmg":0,"heal":25,},
             {"abilityName":"Cat Claws","dmg":50,"heal":0,},
-            {"abilityName":"Cat Eyes","dmg":random(0,67),"heal":0,},
+            {"abilityName":"Cat Eyes","dmg":random.randint(0,67),"heal":0,},
             {"abilityName":"Angry Cat Attack","dmg":75,"heal":0,}
         ]
         self.name = name
         self.health = health
         self.damage = damage
         self.shield = shield
-        self.abilities = abilities
+    
         self.inventory = inventory
 
 #Falcon is sniper
 class Falcon():
-    def __init__(self, name, health, damage, shield, abilities, inventory):
-        abilities = [
+    def __init__(self, name, health, damage, shield, inventory):
+        self.abilities = [
             {"abilityName":"Falcon Dive","dmg":75,"heal":0,},
             {"abilityName":"Falcon Feathers","dmg":20,"heal":0,},
             {"abilityName":"Falcon Screech","dmg":50,"heal":0,},
@@ -38,13 +38,13 @@ class Falcon():
         self.health = health
         self.damage = damage
         self.shield = shield
-        self.abilities = abilities
+        
         self.inventory = inventory
 
 #Snake is poison
 class Snake():
-    def __init__(self, name, health, damage, shield, abilities, inventory):
-        abilities = [
+    def __init__(self, name, health, damage, shield, inventory):
+        self.abilities = [
             {"abilityName":"Snake Venom","dmg":33,"heal":0,},
             {"abilityName":"Snake Crush","dmg":40,"heal":0,},
             {"abilityName":"Snake Fangs","dmg":40,"heal":0,},
@@ -56,16 +56,16 @@ class Snake():
         self.health = health
         self.damage = damage
         self.shield = shield
-        self.abilities = abilities
+       
         self.inventory = inventory
 
 #Frog is support
 class Frog():
-    def __init__(self, name, health, damage, shield, abilities, inventory):
-        abilities = [
+    def __init__(self, name, health, damage, shield, inventory):
+        self.abilities = [
             {"abilityName":"Frog Boost","dmg":33,"heal":0,},
             {"abilityName":"Frog Assist","dmg":20,"heal":30},
-            {"abilityName":"Frog Vial","dmg":10,"heal":random(50,67),}
+            {"abilityName":"Frog Vial","dmg":10,"heal":random.randint(50,67),}
            
             
         ]
@@ -73,13 +73,13 @@ class Frog():
         self.health = health
         self.damage = damage
         self.shield = shield
-        self.abilities = abilities
+       
         self.inventory = inventory
 
 #Shark is tank
 class Shark():
-    def __init__(self, name, health, damage, shield, abilities, inventory):
-        abilities = [
+    def __init__(self, name, health, damage, shield, inventory):
+        self.abilities = [
             {"abilityName":"Shark Attack","dmg":50,"heal":0,},
             {"abilityName":"Shark Fin","dmg":45,"heal":30},
             {"abilityName":"Shark Tail Swing","dmg":25,"heal":25,},
@@ -91,13 +91,13 @@ class Shark():
         self.health = health
         self.damage = damage
         self.shield = shield
-        self.abilities = abilities
+        
         self.inventory = inventory
 
 #Bunny is healer
 class Bunny():
-    def __init__(self, name, health, damage, shield, abilities, inventory):
-        abilities = [
+    def __init__(self, name, health, damage, shield, inventory):
+        self.abilities = [
             {"abilityName":"Bunny Heal","dmg":0,"heal":50,},
             {"abilityName":"Bunny Shield","dmg":25,"heal":30},
             {"abilityName":"Bunny Protection","dmg":30,"heal":25,}
@@ -107,18 +107,18 @@ class Bunny():
         self.health = health
         self.damage = damage
         self.shield = shield
-        self.abilities = abilities
+        
         self.inventory = inventory
 
 #Penguin is damage
 class Penguin():
-    def __init__(self, name, health, damage, shield, abilities, inventory):
-        abilities = [
+    def __init__(self, name, health, damage, shield, inventory):
+        self.abilities = [
             {"abilityName":"Penguin Slap","dmg":44,"heal":0,},
             {"abilityName":"Penguin Punch","dmg":65,"heal":0,},
             {"abilityName":"Penguin Slide","dmg":5,"heal":50,},
             {"abilityName":"Penguin Beak Attack","dmg":35,"heal":0,},
-            {"abilityName":"Penguin Stomp","dmg":random(25,50),"heal":1,}
+            {"abilityName":"Penguin Stomp","dmg":random.randint(25,50),"heal":1,}
 
             
         ]
@@ -126,7 +126,7 @@ class Penguin():
         self.health = health
         self.damage = damage
         self.shield = shield
-        self.abilities = abilities
+        
         self.inventory = inventory
 
 while True:
@@ -167,6 +167,8 @@ print(character.__dict__)
 print("Game starting...")
 time.sleep(2)
 goblin = Enemy("Goblin", 75, 10)
+
+# MY EARS ARE EXPLODING AND IMPLODING
 
 def battle():
     print(f"{character.name} is now fighting {goblin.name}!")
